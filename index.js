32cdb07b250ef905ae6398f50bac16e2269f87ce
Wato Elle <watoelle@gmail.com>
2024-06-28 22:11:09 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = 1;
+Number.isInteger(1);
+
+const num3 = 22;
+const num4 = 9;
+const mod = num3 % num4;
+
+const max = Math.max(1, 4, 12, 16, 20);
+
+
